01285566bfac8f6d70ed81392bd47da19d76e164
yasar-98 <yasar_98@outlook.sa>
2021-08-13 15:35:06 +0300

    product and post front end

--- client/src/views/Activity.js
+++ client/src/views/Activity.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Activity = () => {
-    return (
-        <div>
-            activity
-        </div>
-    )
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function Activity() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+                                        عملنا واحد اثنين ثلاث
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
-
-export default Activity
--- client/src/views/Product.js
+++ client/src/views/Product.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Product = () => {
-    return (
-        <div>
-            Product
-        </div>
-    )
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function Product() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            طاولة <span style={{fontSize:'20px'}}>R61</span>
+          </Typography>
+          
+          <Typography gutterBottom variant="h6" component="h5">
+          99.99
+          <span>NIS</span> 
+ 
+          </Typography>
+
+          <Typography variant="body2" color="textSecondary" component="p">
+                                        منتج خشبي صناعة يدوية 
+          </Typography>
+          <Typography gutterBottom variant="h6" component="h5">
+            الحالة
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <Button variant="contained" color="secondary">
+        الشراء
+    </Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default Product
